4e7bf53da6ecc7721572f311648dd87dd820fd59
theok <Theophile.KARILA@student.isae-supaero.fr>
2022-10-08 18:48:40 +0200

    first commit

---
+++ 3-modules.js
@@ -0,0 +1,7 @@
+// Modules 
+const names = require('./4-names');
+const sayhi = require('./5-utils');
+const data = require('./6-alternative-flavor');
+
+require('./7-mind-grenade');
+sayhi(names.john)
